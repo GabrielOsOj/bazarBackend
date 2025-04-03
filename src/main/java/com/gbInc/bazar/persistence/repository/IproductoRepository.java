5d2a6c81587f94b437e3d29f4991649425c4390b
GabrielOsOj <102068755+GabrielOsOj@users.noreply.github.com>
2025-04-03 00:48:19 -0300

    build: creacion de repositorios

---
+++ src/main/java/com/gbInc/bazar/persistence/repository/IproductoRepository.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+
+package com.gbInc.bazar.persistence.repository;
+
+/**
+ * @author cuent
+ */
+public interface IproductoRepository {
+
+}
---
+++ src/main/java/com/gbInc/bazar/persistence/repository/IventaRepository.java
@@ -0,0 +1,13 @@
+package com.gbInc.bazar.persistence.repository;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+
+/**
+ * @author cuent
+ */
+public interface IventaRepository {
+
+}
